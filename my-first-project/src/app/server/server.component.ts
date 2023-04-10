c32d22cc93ddc437b79f56afaff41bba36986aa7
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2023-04-11 00:00:17 +0200

    creating components + start binding

---
+++ my-first-project/src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
+})
+export class ServerComponent {
+  serverId: number = 1;
+  serverMode: string = 'offline';
+
+  getServerMode() {
+    return this.serverMode;
+  }
+}
